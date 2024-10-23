540b7e4b9e41b99f78966f747876b070b0fec858
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2024-10-23 11:34:24 +0100

    first commit

---
+++ template.sh
@@ -0,0 +1,12 @@
+#prompt the user to input the name of the file to be created
+    echo "Enter the name of the file: "
+read file
+#check if the file has the required extension
+if [[ $file == *.sh ]]; then
+#create the file and input the the default text
+    touch "$file"
+    echo '#!/bin/bash' > "$file"
+nano "$file"
+else
+    echo "please try with .sh extension"
+fi
